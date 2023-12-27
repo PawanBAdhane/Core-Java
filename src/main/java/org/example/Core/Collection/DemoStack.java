53cda10e8499b0150352fa051af87adfa51aeec0
PawanBAdhane <pawanadhane.dev@gmail.com>
2023-12-27 23:45:43 +0530

    Added 60 to stack

--- src/main/java/org/example/Core/Collection/DemoStack.java
+++ src/main/java/org/example/Core/Collection/DemoStack.java
@@ -8,7 +8,7 @@ public class DemoStack {
         Stack s = new Stack();
         s.push(10);
         s.push(20);
-        s.push(30);
+        s.push(60);
 
         System.out.println(s);
         System.out.println(s.peek()); //to get top element
